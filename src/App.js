3c83728a67e779045a54d812fbf0b6734928c460
AHG <ahakangenc92@gmail.com>
2021-07-18 02:00:28 -0500

    Add more routes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
 import { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import * as ROUTES from './constants/routes';
+import useAuthListener from './hooks/use-auth-listener';
+import UserContext from './context/user';
+
 const Login = lazy(() => import ('./pages/Login'));
+const Signup = lazy(() => import ('./pages/Signup'));
+const Dashboard = lazy(() => import ('./pages/Dashboard'));
+const NotFound = lazy(() => import ('./pages/NotFound'));
 
 const App = () => {
+
+  const { user } = useAuthListener();
+
   return (
-    <Router>
-      <Suspense fallback={<p>Loading ...</p>}> 
-        <Switch>
-          <Route path={ROUTES.LOGIN} component={Login} />
-        </Switch>
-      </Suspense>
-    </Router>
+    <UserContext.Provider value={{ user }}>
+      <Router>
+        <Suspense fallback={<p>Loading ...</p>}> 
+          <Switch>
+            <Route exact path={ROUTES.LOGIN} component={Login} />
+            <Route exact path={ROUTES.SIGN_UP} component={Signup} />
+            <Route exact path={ROUTES.DASHBOARD} component={Dashboard} />
+            <Route component={NotFound} />
+          </Switch>
+        </Suspense>
+      </Router>
+    </UserContext.Provider>
   );
 }
 
