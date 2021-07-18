87be37732562a85afabe296b51c93df7af024817
AHG <ahakangenc92@gmail.com>
2021-07-18 01:54:52 -0500

    Login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -48,6 +48,7 @@ const Login = () => {
               type="text"
               placeholder="Email address"
               className="text-sm text-gray base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+              value={emailAddress}
               onChange={({ target }) => setEmailAddress(target.value)}
             />
 
@@ -57,6 +58,7 @@ const Login = () => {
               placeholder="Password"
               autoComplete="off"
               className="text-sm text-gray base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+              value={password}
               onChange={({ target }) => setPassword(target.value)}
             />
 
@@ -72,7 +74,7 @@ const Login = () => {
         </div>
         <div className="flex justify-center items-center flex-col w-full bg-white p-4 rounded border border-gray-primary">
           <p className="text-sm">Don't have an account? {` `}
-            <Link to="/signup" className="font-bold text-blue-medium">
+            <Link to={ROUTES.SIGN_UP} className="font-bold text-blue-medium">
               Sign Up
             </Link>
           </p>
