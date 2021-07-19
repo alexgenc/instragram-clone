15c44fb919a654907e9f5f812f52374d8a95c2e8
AHG <ahakangenc92@gmail.com>
2021-07-19 14:36:44 -0500

    Get photos of followed users

---
+++ src/hooks/use-photos.js
@@ -0,0 +1,33 @@
+import { useState, useEffect, useContext } from 'react';
+import UserContext from '../context/user';
+import { getPhotos, getUserByUserId } from '../services/firebase';
+
+const usePhotos = () => {
+  const [photos, setPhotos] = useState(null);
+
+  const {
+    user: { uid: userId = '' }
+  } = useContext(UserContext);
+
+  useEffect(() => {
+    async function getTimelinePhotos() {
+      const [{ following }] = await getUserByUserId(userId);
+      let followedUserPhotos = [];
+
+      // Does the user actually follow people?
+      if(following.length > 0) {
+        followedUserPhotos = await getPhotos(userId, following);
+      }
+
+      followedUserPhotos.sort((a, b) => b.dateCreated - a.dateCreated);
+      setPhotos(followedUserPhotos);
+    }
+
+    getTimelinePhotos();
+  }, [userId])
+  
+  return { photos };
+ 
+}
+
+export default usePhotos;
