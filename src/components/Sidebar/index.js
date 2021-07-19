02e6fd5c84a1cff00fbf33a31a87ffb394742836
AHG <ahakangenc92@gmail.com>
2021-07-19 14:26:05 -0500

    Sidebar component

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import UseUser from '../../hooks/use-user';
+import User from './user';
+import Suggestions from './suggestions';
+
+const Sidebar = () => {
+  
+  const {
+    user: { docId, fullName, username, userId, following }
+  } = UseUser();
+  
+  return (
+    <div className="p-4">
+      <User username={username} fullName={fullName} />
+      <Suggestions userId={userId} following={following} loggedInUserDocId={docId} />
+    </div>
+  )
+}
+
+export default Sidebar;
