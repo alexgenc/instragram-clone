4fce27070206ac12b14a9ffadb7b2c9d4e67c5f6
AHG <ahakangenc92@gmail.com>
2021-07-19 19:22:04 -0500

    Display header, image, actions, footer, and comments components

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,10 +1,33 @@
 import React, { useRef } from 'react';
 import PropTypes from 'prop-types';
+import Header from './Header';
+import Image from './Image';
+import Actions from './Actions';
+import Footer from './Footer';
+import Comments from './Comments';
 
 const Post = ({ content }) => {
+  
+  const commentInput = useRef(null);
+  const handleFocus = () => commentInput.current.focus();
+  
   return (
-    <div>
-      <p>I am a post</p>
+    <div className="rounded col-span-4 border bg-white border-gray-primary mb-12">
+      <Header username={content.username} />
+      <Image src={content.imageSrc} caption={content.caption} />
+      <Actions 
+        docId={content.docId}
+        totalLikes={content.likes.length}
+        likedPhoto={content.userLikedPhoto}
+        handleFocus={handleFocus}  
+      />
+      <Footer caption={content.caption} username={content.username} />
+      <Comments
+        docId={content.docId}
+        comments={content.comments}
+        posted={content.dateCreated}
+        commentInput={commentInput}
+      />
     </div>
   )
 }
