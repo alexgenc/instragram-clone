5a83ceb2e3f3a2e33ce5ace59e37878ef74f4cc4
AHG <ahakangenc92@gmail.com>
2021-07-23 18:26:22 -0500

    Custom hook returns user pictures from database

--- src/hooks/use-photos.js
+++ src/hooks/use-photos.js
@@ -1,30 +1,21 @@
-import { useState, useEffect, useContext } from 'react';
-import UserContext from '../context/user';
-import { getPhotos, getUserByUserId } from '../services/firebase';
+import { useState, useEffect } from 'react';
+import { getPhotos } from '../services/firebase';
 
-const usePhotos = () => {
+const usePhotos = (user) => {
   const [photos, setPhotos] = useState(null);
-
-  const {
-    user: { uid: userId = '' }
-  } = useContext(UserContext);
 
   useEffect(() => {
     async function getTimelinePhotos() {
-      const [{ following }] = await getUserByUserId(userId);
-      let followedUserPhotos = [];
-
-      // Does the user actually follow people?
-      if(following.length > 0) {
-        followedUserPhotos = await getPhotos(userId, following);
+      // Is there a user, and does the user actually follow other people?
+      if(user?.following?.length > 0) {
+        const followedUserPhotos = await getPhotos(user.userId, user.following);
+        followedUserPhotos.sort((a, b) => b.dateCreated - a.dateCreated);
+        setPhotos(followedUserPhotos);
       }
-
-      followedUserPhotos.sort((a, b) => b.dateCreated - a.dateCreated);
-      setPhotos(followedUserPhotos);
     }
 
     getTimelinePhotos();
-  }, [userId])
+  }, [user?.userId, user?.following])
   
   return { photos };
  
