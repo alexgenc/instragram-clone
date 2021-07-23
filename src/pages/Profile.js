00b37893067eaaeb0fdd173c874c8219bafb39ae
AHG <ahakangenc92@gmail.com>
2021-07-23 18:11:48 -0500

    Display user profile

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useParams, useHistory } from 'react-router-dom';
+import { getUserByUsername } from '../services/firebase';
+import * as ROUTES from '../constants/routes';
+import Header from '../components/Header';
+import UserProfile from '../components/Profile';
 
 const Profile = () => {
-  return (
-    <div>
-      <h1>I'm the profile</h1>
+  const { username } = useParams();
+  const [user, setUser] = useState(null);
+  const history = useHistory();
+
+  useEffect(() => {
+    async function checkUserExists() {
+      const [user] = await getUserByUsername(username);
+      if (user?.userId) {
+        setUser(user);
+      } else {
+        history.push(ROUTES.NOT_FOUND);
+      }
+    }
+
+    checkUserExists();
+  }, [username, history]);
+
+  return user?.username ? (
+    <div className="bg-gray-background">
+      <Header />
+      <div className="mx-auto max-w-screen-lg">
+        <UserProfile user={user} />
+      </div>
     </div>
-  )
+  ) : null;
 }
 
 export default Profile;
